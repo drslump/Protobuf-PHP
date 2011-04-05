7e18e2a43e2c6ba1bd842d64e7062af06536664b
drslump <imv@tid.es>
2011-04-05 22:56:40 +0200

    Small changes to json codec tests

--- tests/Protobuf.Codec.Json.spec.php
+++ tests/Protobuf.Codec.Json.spec.php
@@ -16,20 +16,18 @@ describe "JSON Codec"
     describe "serialize"
 
         it "should serialize a simple message"
-
             $simple = new Tests\Simple();
             $simple->foo = 'FOO';
             $simple->bar = 'BAR';
             $json = Protobuf\Codec\Json::encode($simple);
             $json. should. eq. '{"foo":"FOO","bar":"BAR"}';
-        end;
+        end.
 
     end;
 
     describe "unserialize"
 
         it "should unserialize a simple message"
-
             $json = '{"foo":"FOO","bar":"BAR"}';
             $simple = Protobuf\Codec\Json::decode('Tests\Simple', $json);
             $simple should be instanceof 'Tests\Simple';
