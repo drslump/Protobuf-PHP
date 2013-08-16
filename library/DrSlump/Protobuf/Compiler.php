264f1fa1989e0a8b189927b9cfac785bfcd2c4d3
Ben Aldrich <vrecan@gmail.com>
2013-08-16 16:48:23 -0600

    Add support for underscores in names
    We have fields that start with underscore that get mangled during code generation.

--- library/DrSlump/Protobuf/Compiler.php
+++ library/DrSlump/Protobuf/Compiler.php
@@ -92,7 +92,7 @@ class Compiler
     public function camelize($name)
     {
         return preg_replace_callback(
-                    '/_([a-z0-9])/i',
+                    '/_([a-z0-9_])/i',
                     function($m){ return strtoupper($m[1]); },
                     $name
                  );
